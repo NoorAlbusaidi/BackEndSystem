676613330783d88062e29bc7e935b55cf3df9cac
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-06-23 11:15:35 +0400

    creating program structure

---
+++ flightManagementSystem/FlightContext.cs
@@ -0,0 +1,16 @@
+﻿using flightManagementSystem.models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace flightManagementSystem
+{
+    internal class FlightContext
+    {
+        public List<Aircraft> aircrafts = new List<Aircraft>();
+        public List<Booking> bookings = new List<Booking>();
+        public List<Flight> flights = new List<Flight>();
+        public List<Passenger> passengers = new List<Passenger>();
+        public List<Pilot> pilots = new List<Pilot>();
+    }
+}
--- flightManagementSystem/Program.cs
+++ flightManagementSystem/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace flightManagementSystem
+﻿using flightManagementSystem.models;
+using System.Numerics;
+
+namespace flightManagementSystem
 {
     internal class Program
     {
+        public static FlightContext context = new FlightContext
+        {
+            aircrafts = new List<Aircraft>(),
+            bookings = new List<Booking>(),
+            flights = new List<Flight>(),
+            passengers = new List<Passenger>(),
+            pilots = new List<Pilot>()
+        };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
