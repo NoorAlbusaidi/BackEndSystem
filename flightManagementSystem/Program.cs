b873473d43a99ec706b89af72011ca295af8ed8d
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-06-23 10:56:06 +0400

    Creating Flight Management System project

---
+++ flightManagementSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace flightManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
