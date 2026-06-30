d61aede3b8a55501299d52a4ad7d08d03a45d056
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-06-30 15:44:50 +0400

    done with student class annotation

--- universitySystemProject/Models/Student.cs
+++ universitySystemProject/Models/Student.cs
@@ -1,26 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace universitySystemProject.Models
 {
     internal class Student
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int StudentId { get; set; } //system auto-generated
+
+        [Required]
+        [MaxLength(100)]
         public string StudentName { get; set; } //user input
+
+        [Required]
+        [MaxLength(150)]
         public string StudentEmail { get; set; } //user input
 
-        public string StudentPhoneNum { get; set; }//user input
+        [MaxLength(20)]
+        public string? StudentPhoneNum { get; set; }//user input
 
+        [Required]
         public DateTime StudentBirthDate { get; set; }//user input
 
+        [Required]
+        [Range(2000,2030)]
         public int StudentEnrollmentYear { get; set; }//user input
 
+        [Range(0.0, 4.0)]
+        [DefaultValue(0.0)]
         public decimal StudentGpa { get; set; }//default value
 
-        public Student() {
-
-            StudentGpa = 0;
-        }
     }
 }
