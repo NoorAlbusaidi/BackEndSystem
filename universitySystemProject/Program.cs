bd13597abaefe0a35013b3d03bfc59f7652c4175
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-06-30 15:34:19 +0400

    done with data annotation of Departement and Instructor classes

--- universitySystemProject/Program.cs
+++ universitySystemProject/Program.cs
@@ -5,17 +5,19 @@ namespace universitySystemProject
 {
     internal class Program
     {
+        public static UniversityContext context = new UniversityContext
+        {
+            courses = new List<Course>(),
+            departments = new List<Department>(),
+            students = new List<Student>(),
+            enrollments = new List<Enrollment>(),
+            instructors = new List<Instructor>()
+        };
         static void Main(string[] args)
         {
-                    public static UniversityContext context = new UniversityContext
-                    {
-                        courses = new List<Course>(),
-                        departments = new List<Department>(),
-                        students = new List<Student>(),
-                        enrollments = new List<Enrollment>(),
-                        instructors = new List<Instructor>()
-                    };
 
-    }
+
+        }
     }
 }
+
