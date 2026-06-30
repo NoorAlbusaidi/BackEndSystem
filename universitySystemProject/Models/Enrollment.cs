48a8b9c2d59c43059c64e3484f7e22275d161b18
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-06-30 15:57:19 +0400

    Done with Enrollment class annotation

--- universitySystemProject/Models/Enrollment.cs
+++ universitySystemProject/Models/Enrollment.cs
@@ -1,19 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace universitySystemProject.Models
 {
     internal class Enrollment
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int EnrollmentId { get; set; }
-        public int StudentId { get; set; }
 
+        [ForeignKey(nameof(Student))]
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+
+        [ForeignKey(nameof(Course))]
         public int CourseId { get; set; }
+        public Course Course { get; set; }
+
+        [Required]
         public DateTime EnrollmentDate { get; set; }
 
-        public string FinalGrade { get; set; }
+        [MaxLength(2)]
+        [RegularExpression(@"^(A|A\+|A\-|B|B\+|B\-|C|C\-|C\+|D|D\-|D\+|F)$")]
+        public string? FinalGrade { get; set; }
 
+        [Required]
+        [MaxLength(20)]
+        [DefaultValue("In Progress")]
         public string Status { get; set; }
 
     }
