c0890bfb1abc1852a43298c5f570471d34fd4739
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-07-01 11:58:41 +0400

    done with user class properties

--- ECommerceSystem/Models/User.cs
+++ ECommerceSystem/Models/User.cs
@@ -6,5 +6,15 @@ namespace ECommerceSystem.Models
 {
     internal class User
     {
+        public int UserId { get; set; } //auto-generated
+        public string UserName { get; set; } // user input 
+        public string UserEmail { get; set; } // user input 
+        public string UserPasswordHash { get; set; } //auto-generated
+        public string UserFullName { get; set; } // user input 
+        public string UserPhoneNum { get; set; } // user input 
+        public string UserAddress { get; set; } // user input 
+        public DateTime UserRegistrationDate { get; set; } // auto-generated
+        public bool UserIsActive { get; set; } // default value
+
     }
 }
