29fe5280a705b7c111497648ed8e40b01147483e
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-07-01 12:18:54 +0400

    done with product class properties

--- ECommerceSystem/Models/Product.cs
+++ ECommerceSystem/Models/Product.cs
@@ -6,5 +6,14 @@ namespace ECommerceSystem.Models
 {
     internal class Product
     {
+        public int ProductId { get; set; } //auto-generated
+        public string ProductName { get; set; } //user input
+        public string ProductDescription { get; set; } // user input
+        public decimal ProductPrice { get; set; } // user input
+        public int ProductStockQuantity { get; set; } //user input
+        public string ProductImageUrl { get; set; } // user input
+        public  DateTime ProductCreatedAt { get; set; } //default value
+        public bool ProductIsAvailable { get; set; } //default value
+
     }
 }
