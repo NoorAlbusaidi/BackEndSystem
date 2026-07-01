708361e0cdeb26ba5c5bdbbae8388c903ef54fa3
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-07-01 11:52:26 +0400

    Creating classes of Ecommerce system

---
+++ ECommerceSystem/EcommerceContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceSystem
+{
+    internal class EcommerceContext
+    {
+    }
+}
---
+++ ECommerceSystem/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceSystem.Models
+{
+    internal class Order
+    {
+    }
+}
