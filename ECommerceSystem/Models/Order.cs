6fd4d623345338e82e644b2606320eda996f1101
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-07-01 12:24:10 +0400

    done with order class properties

--- ECommerceSystem/Models/Order.cs
+++ ECommerceSystem/Models/Order.cs
@@ -6,5 +6,13 @@ namespace ECommerceSystem.Models
 {
     internal class Order
     {
+        public int OrderId { get; set; } // auto-generated
+        public DateTime OrderDate { get; set; } // default value
+        public decimal OrderTotalAmount { get; set; } 
+        public string OrderStatus { get; set; } //default value
+        public string OrderShippingAddress { get; set; } //user input
+        public string paymentMethod { get; set; } //user input
+
+
     }
 }
