d6e3237aa3f0906c90ca0156f13c0b58c3f95176
NoorAlbusaidi <nooralhuda.albusaidi@gmail.com>
2026-07-07 08:52:18 +0400

    done with EcommerceContext class

--- ECommerceSystem/EcommerceContext.cs
+++ ECommerceSystem/EcommerceContext.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ECommerceSystem.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace ECommerceSystem
 {
     internal class EcommerceContext: DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=localhost;Database=ECommerceDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
     }
 }
